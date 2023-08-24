ec7e68e910d2942fc4ee08e55e33a9331456dc42
Luis Adiel Martínez <luisadiel99@gmail.com>
2023-08-24 09:44:12 -0600

    Initial commit

---
+++ SteamGameReviews/MainWindow.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace SteamGameReviews
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
