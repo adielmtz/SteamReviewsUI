a8a8c02003c2741387d1236822f484593ad91aa9
Luis Adiel Martínez <luisadiel99@gmail.com>
2023-08-26 16:22:08 -0600

    Increase max reviews limit

--- SteamGameReviews/Steam/SteamConstants.cs
+++ SteamGameReviews/Steam/SteamConstants.cs
@@ -4,8 +4,8 @@ namespace SteamGameReviews.Steam
     {
         public static readonly string ApiEndpoint = "https://store.steampowered.com/appreviews/";
 
-        public static readonly int MinReviews = 20;
-        public static readonly int MaxReviews = 5000;
         public static readonly int MaxReviewsPerRequest = 100;
+        public static readonly int MinReviews = 20;
+        public static readonly int MaxReviews = 10000;
     }
 }
